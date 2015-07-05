6ab872cd9b316ce3da69b299f24fb1392bbe75ea
Michał Glajc <poznet@users.noreply.github.com>
2015-07-06 01:40:36 +0200

    Update CoreBundle.php

--- src/Poznet/CoreBundle/CoreBundle.php
+++ src/Poznet/CoreBundle/CoreBundle.php
@@ -10,18 +10,8 @@ use Poznet\CoreBundle\Classes\Media;
 class CoreBundle extends Bundle
 {
     
-    protected $kernel;
     
-    public function __construct(KernelInterface $kernel=NULL) {
-        if($kernel)
-            $this->kernel = $kernel;
-        
-        $media=new Media;
- 
-     //   $media->checkDir();
- 
-        
- 
-    }
+    
+     
     
 }
