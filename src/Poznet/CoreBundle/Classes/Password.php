8ab8a01b6ab09f1ba3edafd7a7291d74b6622e5d
Michał Glajc <j00s3ph@gmail.com>
2015-07-10 08:30:05 +0200

    funkcja  do zmiany hasla

--- src/Poznet/CoreBundle/Classes/Password.php
+++ src/Poznet/CoreBundle/Classes/Password.php
@@ -6,24 +6,43 @@ namespace Poznet\CoreBundle\Classes;
 class Password  {
 	private $znaki = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789";
 	private $haslo;
- 
+              private $komunikat='';
+     
+              
 
-	public function __construct(){
-		 
-        for ($i = 0; $i < 8; $i++) {
-            $n = rand(0,55);
-            $pass[$i] = $this->znaki[$n];
-        }
-        $haslo=implode($pass,'');
-        $this->haslo=$haslo.'';
-        return $haslo.'';
-}
-	public function __toString(){
+      public function __construct(){
+	 $this->generate();
+                return $haslo.'';
+       }
+       
+       public function generate(){           	 
+                for ($i = 0; $i < 8; $i++) {
+                    $n = rand(0,55);
+                    $pass[$i] = $this->znaki[$n];
+                }
+                $haslo=implode($pass,'');
+                $this->haslo=$haslo.'';
+                return true;
+       }
+       
+       public function __toString(){
 
 		return $this->haslo.'';
-	}
+       }
 
- 
+       public function checkPasswords($old,$new1,$new2){
+           if($new1!=$new2){
+               $this->komunikat.='Niepoprawne powtórzenie Hasła';
+               return false;
+           }
+           
+            if(strlen($new1)<6){
+               $this->komunikat.='Hasło musi mieć minimum 6 znaków';
+               return false;
+           }
+           
+           
+       }
 
 
 }
