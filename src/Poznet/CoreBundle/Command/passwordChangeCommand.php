4235d1e7e5c4866b7e3ca3d8d66d693bb4b67ec6
Pozycjoner.net <michal@glajc.pl>
2015-08-10 23:14:16 +0200

    change admin password command

---
+++ src/Poznet/CoreBundle/Command/passwordChangeCommand.php
@@ -0,0 +1,48 @@
+<?php
+namespace Poznet\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+use Poznet\CoreBundle\Entity\user;
+ 
+class passwordChangeCommand extends ContainerAwareCommand {
+
+    protected function configure()
+    {
+        $this
+            ->setName('sf2core:admin:password')
+            ->setDescription('Changing Admin Password');
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $dialog = $this->getHelper('dialog');
+        $password = $dialog->askHiddenResponse(
+            $output,
+            'New Admin Password ?',
+            false);
+        $user=$em->getRepository('CoreBundle:user')->findOneBy(array('Username'=>'admin'));
+        if($user){
+             $factory = $this->getContainer()->get('security.encoder_factory');
+            try{
+                $encoder = $factory->getEncoder($user);       
+                $encoded = $encoder->encodePassword($password, $user->getSalt());
+                $user->setPassword($encoded);
+                $em->flush();
+            } catch(\RuntimeException $e) {
+          //its  very  lame :)
+        
+            }    
+            
+            $output->writeln('Password Changed');    
+        }else{
+            $output->writeln('Wrong User');    
+        }
+        $output->writeln('Done');
+
+    }
+}
