2e8297162b0cf2cd6f068d201d7bf83d74779404
Pozycjoner.net <michal@glajc.pl>
2015-08-10 23:17:22 +0200

    PSR1 fix

--- src/Poznet/CoreBundle/Command/passwordChangeCommand.php
+++ src/Poznet/CoreBundle/Command/passwordChangeCommand.php
@@ -2,13 +2,11 @@
 namespace Poznet\CoreBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
-use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
-use Poznet\CoreBundle\Entity\user;
  
-class passwordChangeCommand extends ContainerAwareCommand {
+ 
+class PasswordChangeCommand extends ContainerAwareCommand {
 
     protected function configure()
     {
