1f59b500fe5d9b16b19a0defbf20bfabdaf7e66f
Pozycjoner.net <michal@glajc.pl>
2015-09-05 11:24:05 +0200

    create admin command

---
+++ src/Poznet/CoreBundle/Command/CreateAdminCommand.php
@@ -0,0 +1,45 @@
+<?php
+namespace Poznet\CoreBundle\Command;
+
+use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Poznet\CoreBundle\Entity\user;
+ 
+ 
+class CreateAdminCommand extends ContainerAwareCommand {
+
+    protected function configure()
+    {
+        $this
+            ->setName('sf2core:admin:create')
+            ->setDescription('Changing Admin Password');
+    }
+    
+    protected function execute(InputInterface $input, OutputInterface $output)
+    {
+        $em = $this->getContainer()->get('doctrine')->getManager();
+        $user=$em->getRepository('CoreBundle:user')->findOneBy(array('Username'=>'admin'));
+        if(!$user){
+             $user=new user();
+             $factory = $this->getContainer()->get('security.encoder_factory');
+            try{
+                $encoder = $factory->getEncoder($user);       
+                $encoded = $encoder->encodePassword('admin', $user->getSalt());
+                $user->setPassword($encoded);
+                $user->setEmail('me@me.com');
+                $user->setUsername('admin');
+                $em->flush();
+            } catch(\RuntimeException $e) {
+          //its  very  lame :)
+        
+            }    
+            
+            $output->writeln('Konto Za³o¿one');
+        }else{
+            $output->writeln('Admin Juz Istnieje ');
+        }
+        $output->writeln('Done');
+
+    }
+}
