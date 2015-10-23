39a44a49dae8a233d0150c2332243949157a5b64
Michal Glajc <pozycjoner.net@gmail.com>
2015-10-23 12:23:10 +0200

    addet additional methods

--- src/Poznet/CoreBundle/Services/Log.php
+++ src/Poznet/CoreBundle/Services/Log.php
@@ -8,16 +8,35 @@ use Symfony\Component\Security\Core\Authentication\Token\Storage\TokenStorageInt
 
  
 class Log {
+    /**
+     * @var EntityManager
+     */
     private $em;
+    /**
+     * @var TokenStorageInterface
+     */
     private $security;
-    
-    
+
+
+    /**
+     * @param EntityManager $em
+     * @param TokenStorageInterface $security
+     */
     public function __construct(EntityManager $em,TokenStorageInterface $security) {
         $this->em=$em;
         $this->security=$security;
         
     }
-    
+
+    /**
+     * Adds  log  entry
+     *
+     * @param $itemtype
+     * @param $itemid
+     * @param $action
+     * @param $desc
+     * @return bool
+     */
     public function add($itemtype,$itemid,$action,$desc){
          $log=new elog();
          $log->setAction($action);
@@ -30,11 +49,33 @@ class Log {
          $this->em->flush();
          return true;
     }
-    
-    
-   public function getCurrentUserLogs(){
+
+
+    /**
+     * Gets logs of current user
+     * @return mixed
+     */
+    public function getCurrentUserLogs(){
         return $this->em->getRepository("CoreBundle:log")->findByUsername($this->security->getToken()->getUsername());
-    }
+   }
+
+    /**
+     * Get user logs  by user id
+     * @param $id
+     * @return mixed
+     */
+    public function getUserLogsByUserId($id){
+       return $this->em->getRepository("CoreBundle:log")->findById($id);
+   }
+
+    /**
+     * Get user logs by user name
+     * @param $name
+     * @return mixed
+     */
+    public function getUserLogsByUsername($name){
+        return $this->em->getRepository("CoreBundle:log")->findByUsername($name);
+   }
     
     
     
